c556b3300b881cab3bd2a85e38a6afcdc62e34f5
Bertrand Thomas <bertrand@devpro.fr>
2025-07-07 14:25:53 +0200

    Adds host startup wait function
    Introduces a function to wait for the completion of the host startup bootstrap process.
    
    This ensures that subsequent operations requiring a fully initialized host are not executed prematurely.

---
+++ scripts/instruqt/host.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Collection of functions to work with hosts in Instruqt
+
+#######################################
+# Wait for host startup bootstrap to finish
+# Examples:
+#   instruqt_wait_hoststartup
+#######################################
+instruqt_wait_hoststartup() {
+
+  until [ -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
+  do
+    sleep 1
+  done
+}
