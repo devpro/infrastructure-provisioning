56cb0f0a7f63ba6c587703d1405d9d7c1981ba21
Bertrand THOMAS <bertrand@devpro.fr>
2025-10-23 18:46:59 +0200

    Update Docker install on Ubuntu to create user group

--- scripts/docker/docker_install.sh
+++ scripts/docker/docker_install.sh
@@ -15,10 +15,14 @@ docker_install_apt() {
   # adds the repository to Apt sources
   echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
 
   # installs the Docker packages
   sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  # creates user group to avoid sudo
+  sudo usermod -aG docker $USER
+  newgrp docker
 }
